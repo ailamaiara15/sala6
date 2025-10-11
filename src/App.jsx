9a5c286363a5ed19468a8a437241dbf77c61f205
ailamaiara15 <ailamaiara15@gmail.com>
2025-10-11 03:03:57 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vinda à Sala 6!</h1>
+      <p>Aplicativo rodando com sucesso 🚀</p>
+    </div>
+  );
+}
+
+export default App;
