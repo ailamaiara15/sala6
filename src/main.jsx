3dd3275baf939cc7797f761b0559c519377efffd
ailamaiara15 <ailamaiara15@gmail.com>
2025-10-11 03:08:10 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f5f5f5;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  color: #222;
+}
+
+p {
+  color: #555;
+}
